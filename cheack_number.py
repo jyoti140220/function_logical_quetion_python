e10ba64e92e08a51d1172a4b872efdeecb36dd2d
jyoti140220 <71928435+jyoti140220@users.noreply.github.com>
2020-11-15 16:35:16 +0530

    Add files via upload

---
+++ cheack_number.py
@@ -0,0 +1,8 @@
+def cheack_number():
+    if num1%2==0 and num2%2==0:
+        print("dono even h")
+    else:
+        print("dono even nhi h")
+num1=int(input("enter the number"))
+num2=int(input("enter the number"))
+cheack_number()

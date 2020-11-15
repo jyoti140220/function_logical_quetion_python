dfcf20b2e7ded27223fce66829b8224fb2a3349d
jyoti140220 <71928435+jyoti140220@users.noreply.github.com>
2020-11-15 16:34:49 +0530

    Add files via upload

---
+++ check_number_list.py
@@ -0,0 +1,12 @@
+def check_number_list():
+    i=0
+    while i<len(a):
+        if a[i]%2==0 and b[i]%2==0:
+            print("dono even h")
+        else:
+            print("dono even nhi h")
+        print("...........")
+        i=i+1
+a=[2, 6, 18, 10, 3, 75]
+b=[6, 19, 24, 12, 3, 87]
+check_number_list()
